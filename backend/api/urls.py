23475eba1c871289791f748e890aa21a1416b85e
DmtrPhil <dimka.phil@yandex.ru>
2026-07-16 07:55:30 +0300

    добавление роутеров\

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -1,8 +1,19 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 
-router = DefaultRouter()
+from .views import (
+    IngredientViewSet,
+    UserViewSet,
+    RecipeViewSet,
+    TagViewSet
+)
+
+v1_router = DefaultRouter()
+v1_router.register(r'users', UserViewSet, basename='users')
+v1_router.register(r'ingredients', IngredientViewSet, basename='ingredients')
+v1_router.register(r'recipes', RecipeViewSet, basename='recipes')
+v1_router.register(r'tags', TagViewSet, basename='tags')
 
 urlpatterns = [
-    path('', include(router.urls)),
+    path('v1/', include(v1_router.urls)),
 ]
